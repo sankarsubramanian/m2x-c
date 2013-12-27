0050a438c7be74aaab9f6ec5c7587ed6293e4419
Xuejie Xiao <xxuejie@gmail.com>
2013-12-27 22:26:18 +0800

    Add blueprint APIs

--- Makefile
+++ Makefile
@@ -4,10 +4,13 @@ CFLAGS=-O0 -g -Wall -Wextra -std=c89
 LDFLAGS=-lcurl
 M2X_LIB=m2x.a
 
-$(M2X_LIB): feed.o m2x.o client.o parson.o
+$(M2X_LIB): feed.o blueprint.o m2x.o client.o parson.o
 	$(AR) -rcs $@ $^
 
 feed.o: feed.c m2x.h client.h feed.h third_party/parson/parson.h
+	$(CC) -o $@ $(CFLAGS) -c $<
+
+blueprint.o: blueprint.c m2x.h client.h blueprint.h third_party/parson/parson.h
 	$(CC) -o $@ $(CFLAGS) -c $<
 
 m2x.o: m2x.c m2x.h
---
+++ blueprint.c
@@ -0,0 +1,85 @@
+#include <string.h>
+
+#include "m2x.h"
+#include "client.h"
+#include "blueprint.h"
+
+int m2x_blueprint_list(m2x_context *ctx, char **out)
+{
+  return m2x_client_get(ctx, "/blueprints", out);
+}
+
+int m2x_blueprint_view(m2x_context *ctx, const char *blueprint_id, char **out)
+{
+  int ret, len;
+  char *path;
+
+  len = 12 + strlen(blueprint_id) + 1;
+  path = (char *) m2x_malloc(ctx, len);
+  snprintf(path, len, "/blueprints/%s", blueprint_id);
+
+  ret = m2x_client_get(ctx, path, out);
+  m2x_free(path);
+  return ret;
+}
+
+int m2x_blueprint_create(m2x_context *ctx, const char *data, char **out)
+{
+  return m2x_client_post(ctx, "/blueprints", data, out);
+}
+
+int m2x_blueprint_update(m2x_context *ctx, const char *blueprint_id,
+                         const char *data)
+{
+  int ret, len;
+  char *path;
+
+  len = 12 + strlen(blueprint_id) + 1;
+  path = (char *) m2x_malloc(ctx, len);
+  snprintf(path, len, "/blueprints/%s", blueprint_id);
+
+  ret = m2x_client_put(ctx, path, data, NULL);
+  m2x_free(path);
+  return ret;
+}
+
+int m2x_blueprint_delete(m2x_context *ctx, const char *blueprint_id)
+{
+  int ret, len;
+  char *path;
+
+  len = 12 + strlen(blueprint_id) + 1;
+  path = (char *) m2x_malloc(ctx, len);
+  snprintf(path, len, "/blueprints/%s", blueprint_id);
+
+  ret = m2x_client_delete(ctx, path, NULL);
+  m2x_free(path);
+  return ret;
+}
+
+int m2x_json_blueprint_list(m2x_context *ctx, JSON_Value **out)
+{
+  int ret;
+  char *str;
+
+  ret = m2x_blueprint_list(ctx, &str);
+  if (ret == 0) {
+    if (out) { *out = json_parse_string(str); }
+    m2x_free(str);
+  }
+  return ret;
+}
+
+int m2x_json_blueprint_view(m2x_context *ctx, const char *blueprint_id,
+                            JSON_Value **out)
+{
+  int ret;
+  char *str;
+
+  ret = m2x_blueprint_view(ctx, blueprint_id, &str);
+  if (ret == 0) {
+    if (out) { *out = json_parse_string(str); }
+    m2x_free(str);
+  }
+  return ret;
+}
