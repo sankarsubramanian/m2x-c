f4ea430311ee955eb1b8b495ec94d5ac34a1755a
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-12-31 09:58:41 +0800

    Fix datasource after tests

--- datasource.c
+++ datasource.c
@@ -5,9 +5,16 @@
 #include "datasource.h"
 #include "utility.h"
 
-int m2x_datasource_list(m2x_context *ctx, char **out)
+int m2x_datasource_list(m2x_context *ctx, const char *query, char **out)
 {
-  return m2x_client_get(ctx, "/datasources", out);
+  int ret;
+  char *path;
+
+  path = m2x_internal_create_query_path(ctx, "/datasources", query);
+
+  ret = m2x_client_get(ctx, path, out);
+  m2x_free(path);
+  return ret;
 }
 
 int m2x_datasource_view(m2x_context *ctx, const char *datasource_id,
@@ -29,36 +36,38 @@ int m2x_datasource_create(m2x_context *ctx, const char *data, char **out)
 }
 
 int m2x_datasource_update(m2x_context *ctx, const char *datasource_id,
-                          const char *data)
+                          const char *data, char **out)
 {
   int ret;
   char *path;
 
   path = m2x_internal_create_format_string(ctx, "/datasources/%s", datasource_id);
 
-  ret = m2x_client_put(ctx, path, data, NULL);
+  ret = m2x_client_put(ctx, path, data, out);
   m2x_free(path);
   return ret;
 }
 
-int m2x_datasource_delete(m2x_context *ctx, const char *datasource_id)
+int m2x_datasource_delete(m2x_context *ctx, const char *datasource_id,
+                          char **out)
 {
   int ret;
   char *path;
 
   path = m2x_internal_create_format_string(ctx, "/datasources/%s", datasource_id);
 
-  ret = m2x_client_delete(ctx, path, NULL);
+  ret = m2x_client_delete(ctx, path, out);
   m2x_free(path);
   return ret;
 }
 
-int m2x_json_datasource_list(m2x_context *ctx, JSON_Value **out)
+int m2x_json_datasource_list(m2x_context *ctx, const char *query,
+                             JSON_Value **out)
 {
   int ret;
   char *str;
 
-  ret = m2x_datasource_list(ctx, &str);
+  ret = m2x_datasource_list(ctx, query, &str);
   if (ret == 0) {
     if (out) { *out = json_parse_string(str); }
     m2x_free(str);
@@ -87,6 +96,34 @@ int m2x_json_datasource_create(m2x_context *ctx, const char *data,
   char *str;
 
   ret = m2x_datasource_create(ctx, data, &str);
+  if (ret == 0) {
+    if (out) { *out = json_parse_string(str); }
+    m2x_free(str);
+  }
+  return ret;
+}
+
+int m2x_json_datasource_update(m2x_context *ctx, const char *datasource_id,
+                               const char *data, JSON_Value **out)
+{
+  int ret;
+  char *str;
+
+  ret = m2x_datasource_update(ctx, datasource_id, data, &str);
+  if (ret == 0) {
+    if (out) { *out = json_parse_string(str); }
+    m2x_free(str);
+  }
+  return ret;
+}
+
+int m2x_json_datasource_delete(m2x_context *ctx, const char *datasource_id,
+                               JSON_Value **out)
+{
+  int ret;
+  char *str;
+
+  ret = m2x_datasource_delete(ctx, datasource_id, &str);
   if (ret == 0) {
     if (out) { *out = json_parse_string(str); }
     m2x_free(str);
--- datasource.h
+++ datasource.h
@@ -7,20 +7,26 @@
 extern "C" {
 #endif
 
-int m2x_datasource_list(m2x_context *ctx, char **out);
+int m2x_datasource_list(m2x_context *ctx, const char *query, char **out);
 int m2x_datasource_view(m2x_context *ctx, const char *datasource_id,
                         char **out);
 
 int m2x_datasource_create(m2x_context *ctx, const char *data, char **out);
 int m2x_datasource_update(m2x_context *ctx, const char *datasource_id,
-                          const char *data);
-int m2x_datasource_delete(m2x_context *ctx, const char *datasource_id);
+                          const char *data, char **out);
+int m2x_datasource_delete(m2x_context *ctx, const char *datasource_id,
+                          char **out);
 
-int m2x_json_datasource_list(m2x_context *ctx, JSON_Value **out);
+int m2x_json_datasource_list(m2x_context *ctx, const char * query,
+                             JSON_Value **out);
 int m2x_json_datasource_view(m2x_context *ctx, const char *datasource_id,
                              JSON_Value **out);
 
 int m2x_json_datasource_create(m2x_context *ctx, const char *data,
+                               JSON_Value **out);
+int m2x_json_datasource_update(m2x_context *ctx, const char *datasource_id,
+                               const char *data, JSON_Value **out);
+int m2x_json_datasource_delete(m2x_context *ctx, const char *datasource_id,
                                JSON_Value **out);
 
 #if defined(__cplusplus)
